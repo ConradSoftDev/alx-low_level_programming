ef9ff205a0fa5906fc1bb8708ace5d91cb1b20d8
ConradSoftDev <conradsoftdev@gmail.com>
2022-12-08 16:59:29 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,18 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
  *
- *  * main - main block
+ *  * main - Entry point
  *
- *   *return: 0
+ *   *
  *
- *    */
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
-
 {
-
-	        puts("\"Programming is like building a multilingual puzzle");
-
-		        return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+       	return (0);
 
 }
