c36decd7c3335aaae7809e2d75c3b1fae8172516
ConradSoftDev <conradsoftdev@gmail.com>
2022-12-09 17:03:13 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= '2')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putcher('\n');
+	return (0);
+}
