dc55df75cb31b97e029974c083067457443d9b5a
ConradSoftDev <conradsoftdev@gmail.com>
2023-01-10 04:59:09 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,8 +6,8 @@
  * @c: char to assign
  * Description: creat array of size size and assign char c
  * Return: pointer to array, NULL if fail
+ *
  */
-
 char *create_array(unsigned int size, char c)
 {
 	char *str;
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0 || str == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++
+	for (i = 0; i < size; i++)
 		str[i] = c;
 	return (str);
 }
